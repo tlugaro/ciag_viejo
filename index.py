786255dc9bccfabfc8724550fd4eed8c62642fdc
Tomy <tlugaro@agro.uba.ar>
2023-03-19 13:11:10 -0300

    update css letra

--- index.py
+++ index.py
@@ -749,16 +749,16 @@ def home_page():
     temp_suelo = str(round(valores[1][0],1)) + " " + str(unidades[1])
     temp_air =str(round(valores[6][0],1)) + " " + str(unidades[6])
     humedad = str(round(valores[7][0],1)) + " " + str(unidades[7])
-    now= datetime.now()- timedelta(hours=3)
+    now= datetime.now()
     hour= str(now.hour)
-    hour1=str((datetime.now()- timedelta(hours=2)).hour)
+    #hour1=str((datetime.now()- timedelta(hours=2)).hour)
     date = str(now.day) + "/" + str(now.month)
     hours=[47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
     d=[]
     for i in hours:
-        d.append(((datetime.now() - timedelta(hours=3 )- timedelta(hours=i)).hour))
+        d.append(((datetime.now() - timedelta(hours=i)).hour))
 
-    return render_template('ema.html',d=d,hour1=hour1,wind=wind,puntorocio=marchapuntorocio,mrad=marcharad,mhum=marchahum,mpp=ppt,mtsuelo=marchatsuelo,mtmed=marchatmed,tmax=tmax,tmin=tmin, rad=radiacion,pp=pp, temp= temp_suelo, temp2=temp_air, hum=humedad,hora=hour,date=date)
+    return render_template('ema.html',d=d,wind=wind,puntorocio=marchapuntorocio,mrad=marcharad,mhum=marchahum,mpp=ppt,mtsuelo=marchatsuelo,mtmed=marchatmed,tmax=tmax,tmin=tmin, rad=radiacion,pp=pp, temp= temp_suelo, temp2=temp_air, hum=humedad,hora=hour,date=date)
 
 @app.route('/seriesTemporalesSatelitales', methods=["GET", "POST"])
 def seriesTemporalesSateliales():
@@ -785,4 +785,4 @@ def seriesTemporalesSateliales():
             return render_template('seriestemporalessatelites.html',year=year,mes=mes,dia=dia, fecha=date,variab=var, serie=serie, fechas=fechas)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
